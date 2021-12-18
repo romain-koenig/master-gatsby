8de0e26c2dadf87963653926d1fe5b2def94df71
romain-koenig <romain@kromatic.fr>
2021-12-18 22:20:48 +0100

    Just starting-up...

---
+++ starter-files/gatsby/src/pages/index.js
@@ -0,0 +1,10 @@
+//Oncommence par import React. Est-ce toujours nécessaire avec Webpack ?
+import React from 'react';
+
+//Le composant Gatsby va être une simple fonction qui retourne un élément HTML
+
+//ES6 module : on peut l'exporter par défaut à la définition
+export default function HomePage() {
+	return <div>
+	<p>Hey! This is the HomePage</p></div>
+}
